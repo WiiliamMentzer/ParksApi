26b83b1998013a035c3ba102bd8c1996171212f9
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-11-06 15:51:00 -0800

    states controller finished

--- ParksApi/Controllers/StatesController.cs
+++ ParksApi/Controllers/StatesController.cs
@@ -14,5 +14,77 @@ namespace ParksClient.Controllers
     public class StatesController : ControllerBase
     {
         private readonly ParksClientContext _db;
+
+        public StatesController(ParksClientContext db)
+        {
+            _db=db;
+        }
+
+        private bool StateExists(int id)
+        {
+            return _sb.States.Any(e =>e.StateId == id);
+        }
+
+        //Get: api/states/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<State>> GetState(int id)
+        {
+            var state = await _db.States.FindAsync(id);
+
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return state;
+        }
+
+            //Get api/states
+        [HttpGet]
+        public async Task<List<State>> Get(string title, string topic)
+        {
+            IQueryable<State> query = _db.States.AsQueryable();
+
+            if (title != null)
+            {
+                query= query.Where(entry => entry.StateTitle == title);
+            }
+
+            if (topic != null)
+            {
+                query= query.Where(entry => entry.StateTopic == topic);
+            }
+
+        return await query.ToListAsync();
+        }
+
+            //Put: api/States/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, State state)
+    {
+        if (id != state.StateId)
+        {
+            return BadRequest();
+        }
+
+        _db.Entry(state).State = EntityState.Modified;
+
+        try
+        {
+            await _db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+            {
+            if (!StateExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+            }
+        return NoContent();
+        }
     }
 }
